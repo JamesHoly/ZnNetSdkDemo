748de6e95ded8d4263d25b83dda09963153a483e
unknown <375682226@qq.com>
2017-08-06 17:31:55 +0800

    初始化和开启反向监听已开发好。 登录代码已写，但是没测过
    Signed-off-by: unknown <375682226@qq.com>

--- ZN_demo/ZN_demo/Form1.cs
+++ ZN_demo/ZN_demo/Form1.cs
@@ -15,5 +15,36 @@ namespace ZN_demo
         {
             InitializeComponent();
         }
+
+        public int listenCallbakc(int lHandle, string pIp, UInt16 wPort, int lCommand, IntPtr pParam, UInt32 dwParamLen, IntPtr dwUserData)
+        {
+            //MessageBox.Show("listenCallback");
+            IntPtr intPrt = new IntPtr();
+            ZLNetSDKDemo_CSharp.Zlnetsdk.ZLNET_DEVICEINFO deviceInfo = new ZLNetSDKDemo_CSharp.Zlnetsdk.ZLNET_DEVICEINFO();
+            int errCode=-100;
+            int n = ZLNetSDKDemo_CSharp.Zlnetsdk.ZLNET_LoginEx("", 0, "admin", "123456", 8, intPrt, ref deviceInfo, ref errCode);
+            MessageBox.Show("登录信息：" + n);
+            return lHandle;
+        }
+
+
+        /// <summary>
+        /// 开启反向注册监听
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr intPrt = new IntPtr();
+            int n = ZLNetSDKDemo_CSharp.Zlnetsdk.ZLNET_ListenServer("192.168.1.201", 5904, 10000, listenCallbakc, intPrt);
+            MessageBox.Show(n.ToString()); //这里始终返回0
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IntPtr user = new IntPtr();
+            int n = ZLNetSDKDemo_CSharp.Zlnetsdk.ZLNET_Init(null, user);
+            MessageBox.Show(n.ToString());
+        }
     }
 }
